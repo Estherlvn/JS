d2e3311dd849299c76d6aa0e47e65fc862143ad7
Estherlvn <estherlvn@gmail.com>
2024-12-05 15:52:44 +0100

    ajout compteur de card

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -7,6 +7,7 @@ addBtn.addEventListener("click", addTask); // Ajouter une tâche au clic
 // Fonction 'ajouter'
 const taskCard = document.querySelector(".todoCard");
 const tasksContainer = document.querySelector("#todoCards");
+const cardCount = document.querySelector("#cardCount");
 
 function addTask() {
   const newTask = taskCard.cloneNode(true); // clone la carte de tâche
@@ -20,23 +21,33 @@ function addTask() {
   });
 
   tasksContainer.appendChild(newTask); // ajoute une nouvelle task au container
-  updateCount();
+  updateCount(); // met à jour le compteur de cartes
 }
 
 // 2. Suppression d'une todoCard
-//
-//
+
 // écouteur d’évènement sur le bouton pour appeler une fonction 'suppression'
 const delBtn = document.querySelector(".delBtn");
 delBtn.addEventListener("click", function () {
   // supprime une carte au clic
   deleteTask(taskCard); // cible la bonne carte
+  updateCount(); // met à jour le compteur de cartes
 });
 
 // Fonction 'suppression'
 function deleteTask(task) {
   task.remove(); // retire la tâche
+  updateCount(); // met à jour le compteur de cartes
 }
 
 /* 3. Ajouter écouteur d'évènement sur l'élément CLONE dans la fonction d'ajout
 afin de faire fonctionner le bouton suppression sur les nouvelles cards : lignes 15 à 21*/
+
+// 4. Fonction pour mettre à jour le compteur de cartes
+function updateCount() {
+  const numberOfTasks = tasksContainer.querySelectorAll(".todoCard").length;
+  cardCount.textContent = `Number of tasks: ${numberOfTasks}`;
+}
+
+// Initialiser le compteur de cartes au chargement de la page
+updateCount();
